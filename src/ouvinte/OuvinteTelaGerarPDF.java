831b185c564f3e83159234629dcd580ec824b135
HalysonLourran <ysonh68@gmail.com>
2022-07-28 21:37:30 -0300

    PDF criado

--- src/ouvinte/OuvinteTelaGerarPDF.java
+++ src/ouvinte/OuvinteTelaGerarPDF.java
@@ -1,7 +1,11 @@
 package ouvinte;
 
+import java.awt.Desktop;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URI;
+
+import javax.swing.JOptionPane;
 
 import model.CentralDeInformacoes;
 import model.GeradorDeRelatorio;
@@ -12,29 +16,35 @@ import tela.TelaGerarPDF;
 
 public class OuvinteTelaGerarPDF implements ActionListener {
 
-	
 	Persistencia persistencia = new Persistencia();
 	CentralDeInformacoes centralDeInformacoes = persistencia.recuperarCentral();
-	
+
 	public TelaGerarPDF telaGerarPDF;
-	
+
 	public TelaGerarPDF getTelaGerarPDF() {
 		return telaGerarPDF;
 	}
-	
+
 	public OuvinteTelaGerarPDF(TelaGerarPDF tela) {
 		this.telaGerarPDF = tela;
 	}
-	
+
 	@Override
-	public void actionPerformed(ActionEvent e) {	
-		
+	public void actionPerformed(ActionEvent e) {
+
 		telaGerarPDF.dispose();
 		new TelaDeMenu(null);
 	}
 
 	public void actionPerformedGerarPDF(ActionEvent e) {
-		GeradorDeRelatorio.obterProgramacaoDeUmCanal(centralDeInformacoes.getTodosOsProgramas());
-		MensagemPDF.PDFCriado();
+		try {
+			GeradorDeRelatorio.obterProgramacaoDeUmCanal(centralDeInformacoes.getTodosOsProgramas());
+			URI link = new URI("file:///C:/Users/Pichau/eclipse-projets/Agenda/Programa.pdf");
+			Desktop.getDesktop().browse(link);
+			MensagemPDF.PDFCriado();
+
+		} catch (Exception erro) {
+			MensagemPDF.ErroPDF();
+		}
 	}
 }
--- src/personalizedMessage/MensagemPDF.java
+++ src/personalizedMessage/MensagemPDF.java
@@ -7,4 +7,7 @@ public class MensagemPDF {
 	public static void PDFCriado() {
 		JOptionPane.showMessageDialog(null, "PDF Criado");
 	}
+	public static void ErroPDF() {
+		JOptionPane.showMessageDialog(null, "Ocorreu um erro ao criar o PDF");
+	}
 }
